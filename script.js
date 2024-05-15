ea3002812d3bb727eb6ca030c82fbef3a5dc453a
adityathakur309 <rajputru690@gmail.com>
2024-05-15 12:51:42 +0530

    add new change in js and html

--- script.js
+++ script.js
@@ -14,26 +14,26 @@ let humidity = document.querySelector(".humidity")
 let city = "kangra";
 
 let showData = (e) => {
-     city = input.value;
+    city = input.value;
     input.value = ""
     getWeatherData();
-    
-  
-   } 
-  
+
+
+}
+
 // end 
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     showData(e)
 })
 
-let getDate  = (date) => {
+let getDate = (date) => {
     let newDate = new Date(date * 1000)
-   return  new Intl.DateTimeFormat("en-IN",{
-        dateStyle:"full",
-        timeStyle:"short"
-    }).format(newDate) 
-    
+    return new Intl.DateTimeFormat("en-IN", {
+        dateStyle: "full",
+        timeStyle: "short"
+    }).format(newDate)
+
 }
 
 
@@ -41,36 +41,36 @@ let getDate  = (date) => {
 
 // get data from api 
 
-let getWeatherData =  async ()=>{
+let getWeatherData = async () => {
     cityName.innerHTML = "Loading...";
     let apiUrl = `http://api.weatherapi.com/v1/current.json?key=a8836314218a4aaeb17160841241405&q=${city}`
 
     try {
         let response = await fetch(apiUrl);
-       let data = await response.json();
-     let {location,current} = data;
-     cityName.innerHTML = `${city},${location.country}`
-     dateTime.innerHTML = `${getDate(current.last_updated_epoch
-     )}`;
-     weatherFoarcast.innerHTML = `${current.condition?.text}`
-     weatherIcon.innerHTML = `<img src = "http://cdn.weatherapi.com/weather/64x64/day/113.png">`;
-     weathertemp.innerHTML = `${current.temp_c}&deg;C`;
-     windSpeed.innerHTML = `${current.wind_kph}kph`;
-     feelsLike.innerHTML = `${current.feelslike_c}&deg;C`;
-     humidity.innerHTML = `${current.humidity}%`;
-     pressure.innerHTML = `${current.pressure_mb}hpa`
-     
-   
+        let data = await response.json();
+        let { location, current } = data;
+        cityName.innerHTML = `${city},${location.country}`
+        dateTime.innerHTML = `${getDate(current.last_updated_epoch
+        )}`;
+        weatherFoarcast.innerHTML = `${current.condition?.text}`
+        weatherIcon.innerHTML = `<img src = "http://${current.condition?.icon}">`;
+        weathertemp.innerHTML = `${current.temp_c}&deg;C`;
+        windSpeed.innerHTML = `${current.wind_kph}kph`;
+        feelsLike.innerHTML = `${current.feelslike_c}&deg;C`;
+        humidity.innerHTML = `${current.humidity}%`;
+        pressure.innerHTML = `${current.pressure_mb}hpa`
 
-    
-   
-     
-   
-     
-       
+
+
+
+
+
+
+
+
     } catch (error) {
         cityName.innerHTML = "api is not fetching"
-        
+
     }
 }
 getWeatherData();
